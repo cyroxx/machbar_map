f981fcd373302242debbddf993b5a39596e9727b
cyroxx <dev@matthiasjacob.net>
2020-10-27 23:33:03 +0000

    Deploying to gh-pages from master @ 15889f8a1c2d0c06be24649481257d3457b98b4b 🚀

--- floortileset.tsx
+++ floortileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="floortileset" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="floortileset.png" width="256" height="256"/>
+<tileset version="1.4" tiledversion="1.4.2" name="floortileset" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="64" columns="8">
+ <image source="floortileset.png" width="272" height="272"/>
  <tile id="37">
   <properties>
    <property name="collides" type="bool" value="true"/>
